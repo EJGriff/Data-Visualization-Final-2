3c7a92c91f42367de507180ba3f5656426bb17d5
EJGriff <156941396+EJGriff@users.noreply.github.com>
2024-03-02 21:07:22 -0500

    Add files via upload

---
+++ DV0101EN-Final_Assign_Part_2_Questions.py
@@ -0,0 +1,128 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+
+
+data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/historical_automobile_sales.csv')
+
+year_list = [i for i in range(1980, 2024, 1)]
+dropdown_options = [
+    {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
+    {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}]
+
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    html.H1('Automobile Sales Statistics Dashboard',style={'textAlign':'center','color':'#503D36','font-size':24}),#May include style for title
+    html.Div([
+        html.Label("Select Statistics:"),
+        dcc.Dropdown(
+            id='dropdown-statistics',
+            options=dropdown_options,
+            value='Select Statistics',
+            placeholder='Select a report type:'
+        )
+    ]),
+    html.Div(dcc.Dropdown(
+            id='select-year',
+            options=[{'label': i, 'value': i} for i in year_list],
+            value='select-year',
+            placeholder='Select a year'
+        )),
+    html.Div([
+        html.Div(id='output-container', className='chart-grid', style={}),
+        ])
+])
+
+
+@app.callback(
+    Output(component_id='select-year', component_property='disabled'),
+    Input(component_id='dropdown-statistics',component_property='value'))
+
+def update_input_container(selected_statistics):
+    if selected_statistics =='Yearly Statistics': 
+        return False
+    else: 
+        return True
+
+@app.callback(
+    Output(component_id='output-container', component_property='children'),
+    [Input(component_id='dropdown-statistics', component_property='value'), Input(component_id='select-year', component_property='value')])
+
+
+def update_output_container(selected_statistics, selected_year):
+    if selected_statistics == 'Recession Period Statistics':
+        recession_data = data[data['Recession'] == 1]
+        yearly_rec=recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
+        R_chart1 = dcc.Graph(
+            figure=px.line(yearly_rec, 
+                x='Year',
+                y='Automobile_Sales',
+                title="Average Automobile Sales fluctuation over Recession Period"))
+        
+        average_sales = recession_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()                           
+        R_chart2  = dcc.Graph(
+            figure=px.bar(average_sales,
+                x='Vehicle_Type',
+                y='Automobile_Sales',
+                title='Average Number of Vehicles Sold by Type'))
+        
+        exp_rec=recession_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
+        R_chart3 = dcc.Graph(
+            figure=px.pie(exp_rec,
+                values='Advertising_Expenditure',
+                names='Vehicle_Type',
+                title='Total expenditure share by vehicle type'))
+
+        unemp_rec= recession_data.groupby(['Vehicle_Type', 'unemployment_rate'])['Automobile_Sales'].sum().reset_index()
+        R_chart4 = dcc.Graph(
+            figure=px.bar(unemp_rec,
+                x='unemployment_rate',
+                y='Automobile_Sales',
+                color='Vehicle_Type',
+                title="Effect of unemployment rate on vehicle type and sales"))
+
+
+        return [
+            html.Div(className='chart-item', children=[html.Div(children=R_chart1),html.Div(children=R_chart2)],style={'display': 'flex'}),
+            html.Div(className='chart-item', children=[html.Div(children=R_chart3),html.Div(children=R_chart4)],style={'display': 'flex'})
+            ]
+    
+    elif (selected_year and selected_statistics=='Yearly Statistics') :
+        yearly_data = data[data['Year'] == selected_year]
+        yas= data.groupby('Year')['Automobile_Sales'].mean().reset_index()
+        Y_chart1 = dcc.Graph(figure=px.line(yas,
+            x='Year',
+            y='Automobile_Sales',
+            title='Yearly Automobile sales from 1980 to 2023'))
+            
+        Y_chart2 = dcc.Graph(figure=px.line(yearly_data,
+            x='Month',
+            y='Automobile_Sales',
+            title='Total Monthly Automobile from {}'.format(selected_year)))
+
+        avr_data=yearly_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()
+        Y_chart3 = dcc.Graph(figure=px.bar(avr_data,
+            x='Vehicle_Type',
+            y='Automobile_Sales',
+            title='Average Vehicles Sold by Vehicle Type in the year {}'.format(selected_year)))
+
+        exp_data=yearly_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
+        Y_chart4 = dcc.Graph(figure=px.pie(exp_data,
+            values='Advertising_Expenditure',
+            names='Vehicle_Type',
+            title='Advertisement Expenditure by vehicle type'))
+
+        return [
+                html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)],style={'display': 'flex'}),
+                html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)],style={'display': 'flex'})
+                ]
+
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
